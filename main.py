3c2ebdb5e1c06765d02a27e10c970366668e82c9
antonya <148488867+antonyy36@users.noreply.github.com>
2024-01-08 17:30:09 +0300

    Update main.py
    Check this

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('ahahahahahah')
+    print('i changed your program MUAAHAHAHAH))')
 
 
 if __name__ == '__main__':
